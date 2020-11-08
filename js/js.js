97edaf8a6ac943b5b8da77ecf956d858e67e9b7e
nastassiaaa <74069862+nastassiaaa@users.noreply.github.com>
2020-11-08 16:36:43 +0100

    chgt
    csshtmljs

--- js/js.js
+++ js/js.js
@@ -86,3 +86,39 @@
   $('.buttondelete4').on('click', function(){
       $(this).closest("#boxtrklt4").remove();
   });
+
+  $('#img1').click(function() {
+    $('#img1').addClass('jeManime');
+  });
+
+  function activateMaps() {
+
+      let zoomMap = $('.maps-container-inner').ZoomArea({
+          zoomLevel: 1,
+          minZoomLevel: 1,
+          maxZoomLevel: 15,
+          parentOverflow: 'auto',
+          exceptionsZoom: ['marker-all'],
+          hideWhileAnimate: ['map-area', 'marker-all'],
+          virtualScrollbars: false,
+          usedAnimateMethod: 'jquery'
+      });
+
+      resizeFilterPanel();
+  }
+
+  function resizeFilterPanel() {
+      $('.map-filter').removeAttr('style');
+      if (parseInt($('.measurement').css('max-width'), 10) > 768) {
+          $('.map-filter').css('height', $('.maps-container').outerHeight() + 'px');
+      }
+  }
+$(".map-control.map-control-zoomin").trigger("click");
+
+  $(window).on("load", function () {
+      activateMaps();
+  });
+
+  $(window).on("resize", function () {
+      resizeFilterPanel();
+  });
